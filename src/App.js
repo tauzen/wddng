c5d67aec066b27ada68c9d198ddc90b7ff315c6b
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2017-04-17 13:44:06 +0200

    experimenting with firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    props.database.ref('/reservations/').on('value', snapshot => {
+      console.log(snapshot);
+      this.setState({
+        reservations: Object.keys(snapshot.val())
+      });
+    });
+
+    this.state = {
+      reservations: [12, 21]
+    };
+  }
+
   render() {
+    const { signIn, signOut, makeReservation } = this.props;
+    const { reservations } = this.state;
+    const list = reservations.map((item, idx) => {
+      return <p key={idx}>{item}</p>;
+    });
+    console.log(list);
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,9 +37,18 @@ class App extends Component {
         <p className="App-intro">
           in progress
         </p>
+        <button onClick={signIn}>Sign In</button>
+        <button onClick={signOut}>Sign Out</button>
+        <button onClick={makeReservation}>Make Reservation</button>
+        {list}
       </div>
     );
   }
 }
+
+App.PropTypes = {
+  signIn: PropTypes.func.isRequired,
+  signOut: PropTypes.func.isRequired
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
+
 import App from './App';
 import './index.css';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBYTCUKzXhl48rDVZIB66RFNmBDvB2gB6E',
+  authDomain: 'wddng-books.firebaseapp.com',
+  databaseURL: 'https://wddng-books.firebaseio.com',
+  projectId: 'wddng-books',
+  storageBucket: 'wddng-books.appspot.com',
+  messagingSenderId: '495488837968'
+};
+firebase.initializeApp(firebaseConfig);
+window.firebase = firebase;
+
+const id = Math.round(Math.random() * 100);
+
+const signIn = () => {
+  firebase
+    .auth()
+    .signInAnonymously()
+    .then(user => {
+      console.log(user);
+      window.user = user;
+    })
+    .catch(error => {
+      console.error(error.code, error.message);
+    });
+};
+
+const signOut = () => {
+  firebase.auth().signOut().then(() => {
+    console.log('sign out');
+  });
+};
+
+const makeReservation = () => {
+  firebase.database().ref('/reservations/' + id).set({
+    title: 'Title',
+    author: 'Author'
+  });
+};
+
 ReactDOM.render(
-  <App />,
+  <App
+    signIn={signIn}
+    signOut={signOut}
+    makeReservation={makeReservation}
+    database={firebase.database()}
+  />,
   document.getElementById('root')
 );
