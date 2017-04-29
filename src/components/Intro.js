223a238c3c7b9563557fd13394bbfb3355064613
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2017-04-29 13:49:08 +0200

    Intro css and nit fixes

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
 import logo from '../img/logo.svg';
+import './Intro.css';
 
 const Intro = () => (
   <div>
     <div>
       <div>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="Intro-logo" alt="logo" />
         <h2>MK 03/06/2017</h2>
       </div>
-      <div className="App-intro">
+      <div className="Intro-message">
         <p>Drodzy Goście!</p>
         <p>
           Będziemy wdzięczni, jeżeli zamiast kwiatów wręczycie nam książkę
           z dedykacją. Poniżej lista naszych inspiracji. Jeżeli zdecydujecie
           się na którąś pozycję, możecie ją zarezerwować 😊
         </p>
-        <p className="sign">
+        <p className="Intro-signature">
           Do zobaczenia 3go czerwca!<br />Martyna i Krzysztof
         </p>
       </div>
