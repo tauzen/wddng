b0d05cc8276495fb558c7537df5d44bd58cb8fd6
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2017-04-29 11:52:46 +0200

    Styling and intro tweaks

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import BookStore from '../stores/BookStore';
 import BookList from './BookList';
 import logo from '../img/logo.svg';
+import plane from '../img/plane.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,8 +12,22 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h3>MK 03/06/2017</h3>
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>MK 03/06/2017</h2>
+          </div>
+          <div className="App-intro">
+            <p>Drodzy Goście!</p>
+            <p>
+              Będziemy wdzięczni, jeżeli zamiast kwiatów wręczycie nam książkę
+              z dedykacją. Poniżej lista naszych inspiracji. Jeżeli zdecydujecie
+              się na którąś pozycję, możecie ją zarezerwować 😊
+            </p>
+            <p className="sign">
+              Do zobaczenia 3go czerwca!<br />Martyna i Krzysztof
+            </p>
+            <div className="planeSpacer"><img src={plane} alt="plane" /></div>
+          </div>
         </div>
         <BookList store={this.props.bookStore} />
       </div>
